90bacdb8e894885a4328068c02e7617f49a019e7
alexmjn <a.jenkneary@gmail.com>
2021-08-03 16:15:35 -0400

    Additional Project Files

---
+++ first/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <title>Alex Jenkins-Neary PHP</title>
+</head>
+<body>
+   <h1>Alex Jenkins-Neary PHP</h1>
+
+   <pre style="margin-left: 4em">
+   "ASCII ART:"
+
+               *
+            *     *
+            *     *
+            * * * *
+            *     *
+            *     *
+   </pre>
+   <?php
+      echo "The Hash for Alex Jenkins-Neary is";
+      print hash('sha256', 'Alex Jenkins-Neary');
+   ?>
+
+</html>
